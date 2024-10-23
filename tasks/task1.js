6168d05c9353798fbd8f7791b4d0877538696e09
RossYatsiuk <kn23-r.yatsiuk@nubip.edu.ua>
2024-10-23 09:16:55 +0300

    task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,11 +1,23 @@
 // Задача: Написати функцію, яка приймає рядок і замінює всі голосні (a, e, i, o, u) 
-// на певний символ, наприклад *.
+// на певний символ, наприклад *
 
 function replaceVowels(str) {
-  // Ваш код тут
-}
-
-console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
-console.log(replaceVowels("Javascript"));  // Виведе: "J*v*scr*pt"
-
-module.exports = replaceVowels;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+      
+  let result = '';
+  
+  for (let char of str) {
+      if (vowels.includes(char)) {
+          result += '*';
+      } else {
+          result += char;
+      }
+  }
+  
+  return result;
+  }
+  
+  console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
+  console.log(replaceVowels("Javascript"));  // Виведе: "J*v*scr*pt"
+  
+  module.exports = replaceVowels;
